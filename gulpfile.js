49c6107d5e2c1cba47003e41a0b7408a5d75619f
Ramses Garay <senkustone15@gmail.com>
2024-03-25 11:33:20 -0600

    test commit w nm

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
 
@@ -11,6 +13,22 @@ const css = (cb) => {
   cb(); // Callback que avisa a gulp cuando llegamos al final
 };
 
+//imagenes
+
+async function versionWebp(done) {
+  const webp = await import("gulp-webp"); // Manda a traer la dependencia instalada con "npm install --save-dev gulp-webp" desde la terminal"
+
+  const opciones = {
+    quality: 50, // Esto define que tanta calidad se le bajarán a las imágenes
+  };
+
+  src("src/img/**/*.{png,PNG,jpg,JPG}") // Busca recursivamente en todos los archivos y carpetas de la carpeta img con los formatos especificados
+    .pipe(webp.default(opciones)) // Los convierte en formato WEBP y les baja la calidad especificada
+    .pipe(dest("build/img")); // Los guarda en una nueva carpeta
+
+  done(); // Callback que avisa a gulp cuando llegamos al final de la ejecución del script
+}
+
 const dev = (cb) => {
   watch("src/scss/**/*.scss", css);
 
@@ -18,4 +36,5 @@ const dev = (cb) => {
 };
 
 exports.css = css;
-exports.dev = dev;
+exports.dev = parallel(versionWebp, dev);
+exports.versionWebp = versionWebp;
